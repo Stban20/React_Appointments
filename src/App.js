282c68dc76a608068dde72b155847ace0b5d7483
Stban20 <se.ag.2@hotmial.com>
2020-10-01 17:27:35 +0200

    Commit finish project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,67 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Form from "./components/Form";
+import Appointment from "./components/Appointment";
 
 function App() {
+
+  // Appointments in Local Storage
+  let initialAppointments = JSON.parse(localStorage.getItem('appointments')) 
+  if(!initialAppointments){
+    initialAppointments = []
+  }
+  
+  //array appoiments
+  const [appointments, saveAppointment] = useState(initialAppointments)
+
+  // Use effect to do some operations when it changes
+  useEffect( () => {
+    let initialAppointments = JSON.parse(localStorage.getItem('appointments')) 
+    
+    if( initialAppointments){
+      localStorage.setItem('appointments', JSON.stringify(appointments) )
+    } else{
+      localStorage.setItem('appointments', JSON.stringify([]) )
+    }
+  }, [appointments])
+
+  // Function that takes new appoiments and add new appoiment
+  const createAppointment = appointment => {
+    console.log(appointment)
+    saveAppointment([
+      ...appointments,
+      appointment
+    ])
+  }
+
+  // Function to Delete appointment
+  const deleteAppointment = id => {
+    const newAppointments = appointments.filter(appointment => appointment.id !== id)
+    saveAppointment(newAppointments)
+  }
+
+  // Conditional Message to admin or add new appointments
+  const titleNewAppointment = appointments.length === 0 ? 'Any appointment' : 'Admin your Appointments'
+
   return (
     <Fragment>
-      <h1>Manage your appoiments</h1>
+      <h1>Manage your Appointments</h1>
       <div className="container">
         <div className="row">
           <div className="one-half column">
-            <Form></Form>
+            <Form
+              createAppointment = {createAppointment}
+            />
           </div>
-          <div className="one-half column">2</div>
+          <div className="one-half column">
+            <h2>{titleNewAppointment}</h2>
+            {appointments.map(appointment => (
+              <Appointment
+              key = {appointment.id} 
+              appointment = {appointment}
+              deleteAppointment = {deleteAppointment}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </Fragment>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,126 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import PropTypes from 'prop-types'
 
-const Form = () => {
-  return <h1>From the Form</h1>;
+const Form = ({ createAppointment }) => {
+  //State to create appoiments
+  const [appointment, updateAppointment] = useState({
+    pet: "",
+    owner: "",
+    date: "",
+    hour: "",
+    simptoms: "",
+  });
+
+  //Error
+  const [error, updateError] = useState(false);
+
+  //function that execute everytime the user write
+  const updateState = (e) => {
+    updateAppointment({
+      ...appointment,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Extract value from appoiment
+  const { pet, owner, date, hour, simptoms } = appointment;
+
+  // when the user send the form
+  const submitAppointment = (e) => {
+    e.preventDefault();
+
+    //validate form
+    if (
+      pet.trim() === "" ||
+      owner.trim() === "" ||
+      date.trim() === "" ||
+      hour.trim() === "" ||
+      simptoms.trim() === ""
+    ) {
+      updateError(true);
+      return;
+    }
+
+    console.log("Sengind...");
+
+    // Delete previous message
+    updateError(false);
+
+    //Asign  key
+    appointment.id = uuidv4();
+
+    // Create appoiment
+    createAppointment(appointment);
+
+    //Reset form
+    updateAppointment({
+      pet: "",
+      owner: "",
+      date: "",
+      hour: "",
+      simptoms: "",
+    });
+  };
+
+  return (
+    <Fragment>
+      <h2>Create Appointment</h2>
+
+      {error ? <p className="alerta-error">All the info is needed</p> : null}
+
+      <form onSubmit={submitAppointment}>
+        <label>Pet Name</label>
+        <input
+          type="text"
+          name="pet"
+          className="u-full-width"
+          placeholder="Pet Name"
+          onChange={updateState}
+          value={pet}
+        />
+        <label>Owner Name</label>
+        <input
+          type="text"
+          name="owner"
+          className="u-full-width"
+          placeholder="Owner Name"
+          onChange={updateState}
+          value={owner}
+        />
+        <label>Date</label>
+        <input
+          type="date"
+          name="date"
+          className="u-full-width"
+          onChange={updateState}
+          value={date}
+        />
+        <label>Hour</label>
+        <input
+          type="time"
+          name="hour"
+          className="u-full-width"
+          onChange={updateState}
+          value={hour}
+        />
+        <label>Simptoms</label>
+        <textarea
+          className="u-full-width"
+          name="simptoms"
+          onChange={updateState}
+          value={simptoms}
+        ></textarea>
+        <button type="submit" className="u-full-width button-primary">
+          Add Appointment
+        </button>
+      </form>
+    </Fragment>
+  );
 };
+
+Form.propTypes = {
+  createAppointment: PropTypes.func.isRequired  
+}
 
 export default Form;
